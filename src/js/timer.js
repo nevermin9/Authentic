8cf20ce80f81503d8e574f5cc075045e38533b1c
Platoniux <chizzza@gmail.com>
2019-01-22 19:21:55 +0200

    corrected timer

--- src/js/timer.js
+++ src/js/timer.js
@@ -1,5 +1,5 @@
 // Set the date we're counting down to
-var countDownDate = new Date("Dec 31, 2018 23:59:59").getTime();
+var countDownDate = new Date("Dec 31, 2019 23:59:59").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
